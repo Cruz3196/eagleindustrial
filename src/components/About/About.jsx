a5bed75a1de5db53a768887fcbf7864581ade8af
Jose Cruz <cruz@Joses-MacBook-Pro.local>
2024-09-29 16:17:13 -0700

    update

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -10,7 +10,7 @@ const About = () => {
                 <h2>We believe that every great project begins with a solid infrastructure</h2>
                 <p>Our revolutionary engineering, progressive leadership and expert quality of service is how we are able to continuously deliver exceptional work for our clients.</p>
                 <p>The expert leadership at our company, combined with the extensive experience of our highly skilled team, delivers innovative solutions and quality service to our clients, ensuring projects are completed safely, on time, and within budget.</p>
-                <button className="cta-button">OUR COMMITMENT TO SAFETY</button>
+                <button className="cta-button" >OUR COMMITMENT TO SAFETY</button>
             </div>
             <div className='image-content'>
                 <img src={welderImage} alt='Welder About'/>
