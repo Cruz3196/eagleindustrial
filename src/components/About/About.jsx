6867ebc01881786cd72f18b3e58af82cb74a742e
Jose Cruz <cruz@Joses-MacBook-Pro.local>
2024-09-29 17:14:14 -0700

    update

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -7,7 +7,7 @@ const About = () => {
     <section className='about-section'>
         <div className='about-content'>
             <div className='about-text'>
-                <h2>We believe that every great project begins with a solid infrastructure</h2>
+                <h2>We believe that every great project begins with a solid infrastructure, and our esteemed reputation is built on ours. </h2>
                 <p>Our revolutionary engineering, progressive leadership and expert quality of service is how we are able to continuously deliver exceptional work for our clients.</p>
                 <p>The expert leadership at our company, combined with the extensive experience of our highly skilled team, delivers innovative solutions and quality service to our clients, ensuring projects are completed safely, on time, and within budget.</p>
                 <button className="cta-button" >OUR COMMITMENT TO SAFETY</button>
