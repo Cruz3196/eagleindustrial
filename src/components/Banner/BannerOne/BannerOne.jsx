f62c69448e74281740918374f3d6dc8eb0c3c079
Jose Cruz <cruzjos9619@gmail.com>
2024-10-23 13:04:39 -0700

    “update”

--- src/components/Banner/BannerOne/BannerOne.jsx
+++ src/components/Banner/BannerOne/BannerOne.jsx
@@ -5,7 +5,15 @@ const BannerOne = () => {
   return (
     <section className='BannerOne-Section'>
       <div className='BannerOne-Content'>
-
+          <div className='BannerOne-Header'>
+            <h1>Experience and Expertise are Clear in Our Services.</h1>
+          </div>
+          <div className='BannerOne-Content'>
+            <p>We self-perform ASME code repairs and retrofits, civil work, foundation work, trenching, structural bracing, excavation, installation of new pipes, rigging, and heavy equipment setting. </p>
+            <p>
+            From replacing and repairing tanks, valves, pumps, pipes, vessels, and boilers, our experienced team of engineers and construction professionals provide quality and efficiency in all our work.
+            </p>
+          </div>
       </div>
     </section>
   )
