368e0d6be31bcb63505c75649826117382eb20c8
Jose Cruz <cruzjos9619@gmail.com>
2024-10-29 15:54:00 -0700

    update

--- src/components/Footer/FooterOne/FooterOne.jsx
+++ src/components/Footer/FooterOne/FooterOne.jsx
@@ -4,7 +4,11 @@ import './FooterOne.css';
 const FooterOne = () => {
   return (
     <section className='FooterOne-Section'>
-        
+      <div className='FooterOne-Content'>
+        <div className='FooterOne-Logo'>
+          <h2>Eagle Industrial</h2>
+        </div>
+      </div>
     </section>
   )
 }
