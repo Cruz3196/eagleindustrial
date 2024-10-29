e5fc56420f70e7c3c6a612f907c3667d2c178a31
Jose Cruz <cruzjos9619@gmail.com>
2024-10-28 22:24:58 -0700

    update

---
+++ src/components/Buttons/ButtonOne/ButtonOne.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './ButtonOne.css';
+
+const ButtonOne = () => {
+  return (
+    <button className='ButtonOne'>
+        OUR COMMITMENT TO SAFETY
+    </button>
+  )
+}
+
+export default ButtonOne
