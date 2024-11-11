ccdd38bb248137344040747cb9749d78b540570f
Jose Cruz <cruzjos9619@gmail.com>
2024-11-11 11:02:15 -0800

    update

--- src/components/Hero/HeroTwo.jsx
+++ src/components/Hero/HeroTwo.jsx
@@ -10,7 +10,17 @@ const HeroTwo = () => {
         </div>
         <div id='HeroTwo-Borderline'>
         </div>
-        
+        <div className='HeroTwo-Content'>
+          <p>
+            No matter the size, complexity or turnaround time of your project, we are fully equipped for any challenge.  
+          </p>
+          <p>
+            The expert leadership at Eagle Industrial and the collective experience of our highly skilled team provides our clients with innovative solutions and quality service ensuring projects are completed safely, on time and on budget.  
+          </p>
+          <p>
+            Expand your Our clients interact directly with our leadership team who continuously strive to provide cost-effective solutions to technically challenging initiatives, and deliver exceptional results. We have saved clients upwards of 30% on construction projects leveraging our thorough pre-planning process and groundbreaking engineering. 
+          </p>
+        </div>
       </div>
     </section>
   )
