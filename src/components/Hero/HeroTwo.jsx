1ec3cb5c4abd6e2e27040ac0c0dac4cd6bbd2a84
Jose Cruz <cruzjos9619@gmail.com>
2024-11-11 10:13:56 -0800

    update

---
+++ src/components/Hero/HeroTwo.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const HeroTwo = () => {
+  return (
+    <section className='HeroTwo-Section'>
+      <div className='HeroTwo-Container'>
+        <div className='HeroTwo-Header'>
+          expert leadership on every project 
+        </div>
+      </div>
+    </section>
+  )
+}
+
+export default HeroTwo
--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,8 +1,10 @@
-import React from 'react'
+import React from 'react';
+import { HeroTwo } from '../components';
+
 
 const About = () => {
   return (
-    <div>About</div>
+    <HeroTwo/>
   )
 }
 
