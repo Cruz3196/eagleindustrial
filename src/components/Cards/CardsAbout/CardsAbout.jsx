d6476dc88f5f1bd5296f78c376615c6c57183d33
Jose Cruz <cruzjos9619@gmail.com>
2024-11-11 22:44:49 -0800

    update

--- src/components/Cards/CardsAbout/CardsAbout.jsx
+++ src/components/Cards/CardsAbout/CardsAbout.jsx
@@ -1,16 +1,27 @@
 import React from 'react';
+import AboutOne from '../../../Assets/AboutOne.jpg';
+import AboutTwo from '../../../Assets/AboutTwo.jpg';
+import AboutThree from '../../../Assets/AboutThree.jpg';
 import './CardsAbout.css';
+
+const images = [
+    {src: AboutOne, alt: 'image1'},
+    {src: AboutTwo, alt: 'image2'},
+    {src: AboutThree, alt: 'image3'}
+];
 
 const CardsAbout = () => {
   return (
     <section className='CardsAbout-Section'>
         <div className='CardsAbout-Container'>
-            <div className='CardsAbout-Content'>
-                Cards 
-            </div>
+            {images.map((image, index) => (
+              <div key={index} className='CardsAbout-Card'>
+                <img className='CardsAbout-Image' src={image.src} alt={image.alt} />
+              </div>
+            ))}
         </div>
     </section>
-  )
-}
+  );
+};
 
 export default CardsAbout; 
