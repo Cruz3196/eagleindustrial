064b910ff47ea6291e11482e4e727c04cec8c3cd
Jose Cruz <cruzjos9619@gmail.com>
2024-11-11 23:14:58 -0800

    update

--- src/components/Banner/BannerThree/BannerThree.jsx
+++ src/components/Banner/BannerThree/BannerThree.jsx
@@ -12,13 +12,14 @@ const BannerThree = () => {
             <p>
             Eagle Industrial is a dynamic leader in industrial construction & project management servicing a multitude of industries since 2008.
             </p>
-            <h2>
-            Safety is our core priority and at the foundation of everything we build.
+            <h2 className='BannerThree-Heading1'>
+            Safety is our core priority and at the foundation of <br/> everything we build.
             </h2>
             <p>
             Our revolutionary engineering, progressive leadership and expert quality of service is how we are able to continuously deliver exceptional work for our clients, and what makes us a trusted source and sought after construction partner.
             </p>
-            <h2>Our work is raising the bar to build a stronger, safer, more sustainable future.
+            <h2 className='BannerThree-Heading2'>
+              Our work is raising the bar to build a stronger, safer, more sustainable future.
             </h2>
         </div>
     </section>
