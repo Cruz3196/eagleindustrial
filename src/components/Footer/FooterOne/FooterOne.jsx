8ed16225298334030273fd8af59de315d468d892
Jose Cruz <cruzjos9619@gmail.com>
2024-11-14 21:50:06 -0800

    update

--- src/components/Footer/FooterOne/FooterOne.jsx
+++ src/components/Footer/FooterOne/FooterOne.jsx
@@ -5,19 +5,17 @@ import FooterOneButton from '../../Buttons/FooterOneButton';
 const FooterOne = () => {
   return (
     <section className='FooterOne-Section'>
-      <div className='FooterOne-Container'>
-        <div className='FooterOne-Content'>
-          <div className='FooterOne-Logo'>
-            <h2>Eagle Industrial</h2>
-          </div>
-          <div id='borderline'>
-          </div>
-            <h2 className='FooterOne-Ending'>
-              Quality and Safety, Engineered.
-            </h2>
-            <FooterOneButton/>
+      <div className='FooterOne-Content'>
+        <div className='FooterOne-Logo'>
+          <h2>Eagle Industrial</h2>
         </div>
+        <div id='borderline'>
+        </div>
+          <h2 className='FooterOne-Ending'>
+            Quality and Safety, Engineered.
+          </h2>
       </div>
+          <FooterOneButton/>
     </section>
   )
 }
