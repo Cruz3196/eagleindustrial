286732094583a33de5735c82f4c6e621a1e111ea
Jose Cruz <cruzjos9619@gmail.com>
2024-11-18 20:02:51 -0800

    ServiceUpdate

--- src/components/Cards/CardsService/CardsService.jsx
+++ src/components/Cards/CardsService/CardsService.jsx
@@ -9,26 +9,57 @@ const services = [
         src: ServiceOne,
         alt: 'serviceOne',
         title: 'Facility Construction',
-        info: ['Compressor stations', 'Gas lift installations', 'Tank battery/central tank battery']
+        info: ['Compressor stations', 
+              'Gas lift installations', 
+              'Tank battery/central tank battery', 
+              'Pump Stations', 
+              'Process facilities',
+              'Cryogenic facilities',
+              'Amine facilities',
+              'Lpg storage/handling facilities',
+              'Metering facilities',
+              'Saltwater disposal facilities (swd)',
+              'Tank terminals and storage facilities'
+            ]
     },
     {
         src: ServiceTwo,
         alt: 'serviceTwo',
         title: 'Pipeline Maintenance & Integrity',
-        info: ['Anomaly verifications and repairs', 'Full assistance with caliper pig runs']
+        info: ['Anomaly verifications and repairs', 
+                'Full assistance with caliper pig runs',
+                'Recoating',
+                'Pipelines repair, modification, and replacement',
+                'Weld Repairs',
+                'Fabrication & installation of launchers, receivers, valve sets, & risers',
+                'Hydrostatic testing',
+                'Replacement of fittings, valves, and drips',
+                'Row maintenance and restoration'
+              ]
     },
     {
         src: ServiceThree,
         alt: 'serviceThree',
         title: 'Pipeline Construction',
-        info: ['Steel', 'Poly', 'Flexsteel']
+        info: ['Steel', 
+              'Poly', 
+              'Flexsteel',
+              'Fiberglass',
+              'Fiber Spur',
+              'Fabrication pipeline & structural',
+              'Fabrication Valve & Valve Set',
+              'Integrity Work & Anomaly Repair',
+              'Sandblasting & Painting',
+              'Zap-lock',
+              'Bores'
+            ]
     }
 ];
 
 const CardsService = () => {
   return (
     <section className='CardsService-Section'>
-        <h2 className='CardsService-Title'>Our Services.</h2>
+        <h2 className='CardsService-Header'>Our Services.</h2>
         <div className='CardsService-Container'>
             {services.map((service, index) => (
               <div key={index} className='CardsService-Card'>
