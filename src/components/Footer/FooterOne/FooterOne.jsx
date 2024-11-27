e99ff1f17a9f0befb823e491d15ba12bbfb070b1
Jose Cruz <cruzjos9619@gmail.com>
2024-11-26 21:10:00 -0800

    update

--- src/components/Footer/FooterOne/FooterOne.jsx
+++ src/components/Footer/FooterOne/FooterOne.jsx
@@ -1,11 +1,26 @@
 import React from 'react';
+import { useLocation } from 'react-router-dom';
 import './FooterOne.css';
 import FooterOneButton from '../../Buttons/FooterOneButton';
 
 const FooterOne = () => {
+  const location = useLocation();
+
+  // check if the current path matches "About" or "Safety"
+  const footerTwo = location.pathname === '/About' || location.pathname === '/Safety';
+
   return (
-    <section className='FooterOne-Section'>
-      <div className='FooterOne-Content'>
+    <section className='FooterOne-Section'
+    style={{
+      height: footerTwo ? '60vh' : '46vh', // Conditional height
+    }}
+    >
+      <div 
+        className='FooterOne-Content'
+        style={{
+          marginTop: footerTwo ? '100px' : '0px',
+        }}
+      >
         <div className='FooterOne-Logo'>
           <h2>Eagle Industrial</h2>
         </div>
