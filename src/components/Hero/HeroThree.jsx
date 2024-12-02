cdbb90b4026b66a62ab9f8133f8b048b784c9557
Jose Cruz <cruzjos9619@gmail.com>
2024-12-01 16:59:37 -0800

    update

--- src/components/Hero/HeroThree.jsx
+++ src/components/Hero/HeroThree.jsx
@@ -4,8 +4,12 @@ import './HeroThree.css';
 const HeroThree = () => {
   return (
     <div className='HeroThree-Container'>
-        <h1 className='HeroThree-text'>Our Core Commitment to Safety
-        100/0 = 100% Accountability / 0% Excuses
+        <h1 className='HeroThree-text'>
+          Our Core Commitment to <br/> Safety
+          <br/>
+          <span className='HeroThree-subtext'>
+            100/0 = 100% Accountability / 0% Excuses
+          </span>
         </h1>
     </div>
   )
